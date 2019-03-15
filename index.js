bfc2769d7ce22f276bf234247b0f0b92c53c66a6
Nicolas Hosteins <n.hosteins@gmail.com>
2019-03-15 10:01:49 +0100

    nettoyage html et creation js

---
+++ index.js
@@ -0,0 +1,49 @@
+const students = [{
+    nom: 'CHARLEMAGNE',
+    prenom: 'Julie',
+    langage : 'JS',
+    age : '24 ans',
+    Email: 'julie.charlemagne@live.fr',
+    image1 : 'julie.jpg',
+    image2 : 'julie2.jpg',
+    présentation: `Julie, 24 ans, j\'ai grandi a Pau, depuis le lycée je cotoie la région de Bordeaux, étant sur le bassin d\'arcachon. Je suis venue sur Bordeaux après mon bac il y a 5 ans, et j\'étais à la recherche de ma voie profetionelle jusqu\'a octobre 2018.
+    J\'ai fais un bac S spé Math en 2013. J\'ai voulu continuer mes études dans la voie scientifque mais je me suis perdue dans des options qui ne me convenait pas.
+    Vient ensuite une recherche de profession qui me plairait, en cherchant dans mes hobits, et ce fu désilusion en désilusion, et c\'est suite à une remise en question et un bilan de compétences qu\'il a été mis en évidence que mon profil convenait au developpement informatique.
+    Suite à celà je me suis mise à coder toute seule et à suivre des tutos, il s'est averé que je m\'y suis beaucoup plu.
+    J'ai alors cherché une formation diplomante, et me voilà à la Wild Code School.
+    Je suis contente d\'être là aujourd\'hui et j\'espère que mon avenir sera plus clair de que passé.`,
+    },{
+    nom: 'DANG',
+    prenom: 'Elneris',
+    langage: 'PHP',
+    age : '29 ans',
+    Email: 'elneris.dang@gmail.com',
+    image1 : 'elneris.jpg',
+    image2 : 'elneris2.jpg',
+    présentation:`Moi c'est Elnéris, j'ai 29 ans, d'origine vietnamienne et je suis en pleine reconversion depuis février 2019 !
+    J'ai fais des études en électrotechnique et reçu mon bac en 2009. Suite à celà, je n'ai malheureusement pas pu continuer mes études comme je l'aurai souhaiter. Cependant, aujourd'hui, je suis bien content d'avoir suivi ce bac qui me permettre d'avoir un peu plus de facilité en Algo :p
+    Pendant l'année de mon bac, je travaillais déjà en parallèle dans la restauration rapide : Mcdo !
+    Je suis resté 10 ans dans la restauration jusqu'en octobre 2018, juste avant de découvrir la Wild code School. `
+    },{
+    nom: 'HAMON',
+    prenom: 'Martin',
+    langage: 'JS',
+    age : '23 ans',
+    Email: 'martinh@outlook.fr ',
+    image1 : 'martin.jpg',
+    image2 : 'martin2.jpg',
+    présentation:`Salut ! moi c'est Martin, Originaire de Nantes je suis venu me perdre à Bordeaux pour suivre ma copine et changer de métier !
+
+    J'ai fait des études en électrotechnique Bac, BTS, licence, en travaillant toujours en alternance, après ces études, j'ai travaillé pour la même entreprise. Cependant, je ne me voyais pas faire ce travail toute ma vie, et le domaine du développement Web me semblait intéressant, résultat après quelques mois de recherche me voilà à la Wild Code School !` 
+    },{
+    nom: 'HOSTEINS',
+    prenom: 'Nicolas',
+    langage: 'JS'
+    age : '29 ans',
+    Email: 'n.hosteins@gmail.com',
+    image1 : 'nico.jpg',
+    image2 : 'nicolas.jpg',
+    présentation:`Après un diplome d'environnement en 2002, j'ai travaillé un an dans ce domaine. Pendant la période de chomage suivante, je me suis inscrit en bénévole dans une salle de concert. J'ai travaillé ainsi 15 ans dans ce domaine des musiques amplifiées, sans le chercher vraiment, mais avec passion.
+    Aujourd'hui, la passion s'étant érodée, je me reconverti dans le développement web, métier plus traditionnel mais certainement tout aussi interessant. `
+    }
+]
